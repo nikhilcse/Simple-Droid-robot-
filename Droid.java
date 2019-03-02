72a5cd643a4af40bf135353ff78e41648debe9d9
Nikhil Kumar <46191967+nk108@users.noreply.github.com>
2019-03-02 22:47:31 +0530

    Droid.java

---
+++ Droid.java
@@ -0,0 +1,50 @@
+//Here we have illustrated the significance of various methods to perform the workings of the Droid (Robot).
+class Droid{
+  int batteryLevel;
+ Droid(){
+    batteryLevel=100;
+ //Now every time a Droid is created, its battery level will be at 100 percent.
+    }
+  public void activate(){
+    System.out.println("Activated! How can I help You?");
+   
+  batteryLevel=batteryLevel-5;
+  System.out.println("Battery Level is:"+ batteryLevel+" percent.");
+}
+public void chargeBattery(int hours){
+System.out.println("Droid Charging...");  
+  batteryLevel=batteryLevel+hours;
+  if(batteryLevel>100){
+    batteryLevel=100;
+    System.out.println("Battery Level is:"+batteryLevel+" percent.");
+  }
+      
+ else{
+   System.out.println("The Battery Level is:"+batteryLevel+" percent.");
+ }
+}
+//Let's provide a way for users to charge their Droid. 
+public int checkBatteryLevel(){
+  System.out.println("The batteryLevel is:"+batteryLevel+" percent.");
+  return(batteryLevel);
+}
+  public void hover(int feet){
+    if(feet>2){
+      System.out.println("Error! I cannot hover above 2 feet.");
+    }
+    else{
+      System.out.println("Hovering...");
+      }
+    batteryLevel=batteryLevel-20;
+    System.out.println("The Battery Level is:"+batteryLevel+" percent.");
+  }
+
+  public static void main(String args[]){
+    Droid Droid=new Droid();
+    Droid.activate();
+    Droid.chargeBattery(5);
+    Droid.hover(2);
+    
+  }
+  
+}
